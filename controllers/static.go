791240f96b3ea10298eeacb3ff8d2c4b2e8deb22
Olexandr Shaposhnyk <oshaposhnyk@intelliboard.net>
2024-05-22 20:41:32 +0300

    Made the FAQ page use dynamically provided questions

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/oshaposhnyk/lenslocked/views"
@@ -9,5 +10,26 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer: `We have support staff ansering emaiils 24/7, 
+			though response times may be a bit slower on weekends..`,
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
